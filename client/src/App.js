8ff90bb63b4719b233363ae2deb1ffe3c7411da0
robester0403 <robkso@gmail.com>
2022-10-21 11:25:17 -0400

    installed sass MUI router dom and folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,23 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import MainPage from "./pages/MainPage";
+import AllProducts from "./views/AllProducts";
+import ProductDetails from "./views/ProductDetails";
+
 function App() {
-  return <div></div>;
+  document.title = "storeapp";
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<MainPage />}>
+          <Route path="allproducts" element={<AllProducts />} />
+          <Route path="productdetails" element={<ProductDetails />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div>Mui components or normal form here?</div>;
+};
+
+export default Login;
