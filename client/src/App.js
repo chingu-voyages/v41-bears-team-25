f8450e9efe909a2e55eeab29bdb5f94cb392dae2
robester0403 <robkso@gmail.com>
2022-10-23 14:33:36 -0400

    setup unrefactored react query on all products page

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,24 @@ import MainPage from "./pages/MainPage";
 import ReduxTest from "./pages/ReduxTest";
 import AllProducts from "./views/AllProducts";
 import ProductDetails from "./views/ProductDetails";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 function App() {
   document.title = "storeapp";
+  const queryClient = new QueryClient();
   return (
-    <div>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<MainPage />}>
-          <Route path="allproducts" element={<AllProducts />} />
-          <Route path="productdetails" element={<ProductDetails />} />
-        </Route>
-        <Route path="/reduxtest" element={<ReduxTest />} />
-      </Routes>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<MainPage />}>
+            <Route path="allproducts" element={<AllProducts />} />
+            <Route path="productdetails" element={<ProductDetails />} />
+          </Route>
+          <Route path="/reduxtest" element={<ReduxTest />} />
+        </Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- client/src/views/AllProducts.jsx
+++ client/src/views/AllProducts.jsx
@@ -1,7 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { useState } from "react";
 
 const AllProducts = () => {
-  return <div>AllProducts</div>;
+  const [appData, setAppData] = useState({});
+  const { isLoading, error, data } = useQuery(["repoData"], () =>
+    fetch("https://api.github.com/repos/tannerlinsley/react-query").then(
+      (res) => res.json().then(setAppData(data))
+    )
+  );
+
+  return (
+    <>
+      {isLoading && <div>loading app</div>}
+      {error && <div>error loading app</div>}
+      <div>AllProducts</div>
+    </>
+  );
 };
 
 export default AllProducts;
