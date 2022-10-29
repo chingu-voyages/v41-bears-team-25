74016cd5e689b5f07bdd78fbacaf82ff2125e44f
robester0403 <robkso@gmail.com>
2022-10-29 14:43:54 -0400

    Changes to assets, added typography and general centering styling for login page.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,24 +3,31 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import GoogleIcon from "@mui/icons-material/Google";
+import { Typography } from "@mui/material";
+import whiteVase from "../assets/images/WhiteVase.jpg";
 
 const Login = () => {
   return (
-    <React.Fragment>
+    <>
       <CssBaseline />
-      <Container maxWidth="sm">
-        <img
-          src="https://images.unsplash.com/photo-1633000116322-d7f5cb7d3ebb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1051&q=80"
-          alt="White vase"
-          height="500"
-        />
-        <h1>Welcome to App Name</h1>
-        <h3>We make it easy for everyone to sell their stuff.</h3>
+      <Container
+        maxWidth="sm"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <img src={whiteVase} alt="White vase" height="500" />
+        <Typography variant="h1">Welcome to App Name</Typography>
+        <Typography variant="h3">
+          We make it easy for everyone to sell their stuff.
+        </Typography>
         <Button variant="outlined" startIcon={<GoogleIcon />}>
           Sign in with Google
         </Button>
       </Container>
-    </React.Fragment>
+    </>
   );
 };
 
